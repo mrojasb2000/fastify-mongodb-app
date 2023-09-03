2db4117505f47286f353d10034175b362de42dbd
maalroja <maalroja@bancolombia.com.co>
2023-09-03 19:46:07 -0300

    chore: add product api crud routes

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -12,6 +12,29 @@ const routes = [{
         const { id } = request.params;
         reply.send(`Products with ID: ${id}`);
     }
-}]
+},
+{
+    method: 'POST',
+    url: '/products',
+    handler: (request, reply) => {
+        reply.send(`creating a new product`);
+    }
+},
+{
+    method: 'PUT',
+    url: '/products/:id',
+    handler: (request, reply) => {
+        const { id } = request.params;
+        reply.send(`Updating a products with ID: ${id}`);
+    }
+},
+{
+    method: 'DELETE',
+    url: '/products/:id',
+    handler: (request, reply) => {
+        const { id } = request.params;
+        reply.send(`Removing a products with ID: ${id}`);
+    }
+},]
 
 module.exports = routes;
